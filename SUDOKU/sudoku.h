f294ca61a5a3a4e516baa6dd5baf254027f792c0
dungdunght <dungnd560@gmail.com>
2015-09-18 01:17:56 +0300

    Добавил коментарии в sudoku.h

--- SUDOKU/sudoku.h
+++ SUDOKU/sudoku.h
@@ -4,12 +4,37 @@
 #include <string.h>
 #include <fstream>
 
-const int MAXK = 100;
+const int MAXK = 100; //Максимальный размер головоломки
 
+/*!
+* Вводить головоломку из файла input.txt
+* \param[out] k - размер головоломку
+* \param[out] tableSudoku[MAXK][MAXK] - массив сохнраняет головоломку
+*/
 void InputMatrixSudoku(int &k, int tableSudoku[MAXK][MAXK]);
 
+/*!
+* Выводить головоломку на файл output.txt
+* \param[in] k - размер головоломку
+* \param[in] tableSudoku[MAXK][MAXK] - массив сохнраняет головоломку
+*/
 void OutputMatrixSudoku(int k, int tableSudoku[MAXK][MAXK]);
 
+
+/*!
+* Выводить сообщение о неправильной головоломке на файл output.txt
+* \param[in] k - размер головоломку
+* \param[in] tableSudoku[MAXK][MAXK] - массив сохнраняет головоломку
+*/
 void OuputErrorSudoku(int k);
 
+
+/*!
+* Оброботать рекурсию для того чтобы заполнить головоломку
+* \param[in] k - размер головоломку
+* \param[in] tableSudoku[MAXK][MAXK] - массив сохнраняет головоломку
+* \param[in] i,j - текущая позиция у незаполненной клетки, которой надо рассматривать
+* \return true - головоломка решила 
+* \return false -головоломка не решила
+*/
 bool recusionSudoku(int k, int tableSudoku[MAXK][MAXK], int i, int j);
