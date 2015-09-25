5893d26060871349cd6d5c75349ddcbff8e9076b
dungdunght <dungnd560@gmail.com>
2015-09-25 10:56:56 +0300

    вернуться флаг о неправильно входе данных

--- SUDOKU/sudoku.cpp
+++ SUDOKU/sudoku.cpp
@@ -63,6 +63,8 @@ bool checkInputSudoku(int k,int tableSudoku[MAXK][MAXK],int &i_false,int &j_fals
 				{
 					i_false = i;
 					j_false = j;
+					return 0;
+				}
 				checkNumber[tableSudoku[i][j]] = false;
 			}
 		}
